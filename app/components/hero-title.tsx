da7d3cb9e5f48c2230397e6bf2c4437d2fdedb08
Rudi Hinds <rudihinds@gmail.com>
2024-06-20 00:06:49 +0100

    fix typography and margins in title

--- app/components/hero-title.tsx
+++ app/components/hero-title.tsx
@@ -33,24 +33,25 @@ function FlipWordsTitle() {
 
   return (
     <div className="grid grid-cols-1 lg:grid-cols-2 h-full relative pt-6">
-      <div className="h-full flex justify-center items-start flex-col gap-y-4 lg:gap-y-2 lg:pl-20 ">
-        <div className="mx-auto lg:mx-0 px-4 lg:px-0 text-6xl lg:text-5xl xl:text-7xl font-bold xl:font-normal text-center lg:text-left text-neutral-600 dark:text-neutral-300 shadow-2xl w-full mb-6 xl:mb-4   ">
-          Put your systems <br /> on autopilot with AI
+      <div className="flex justify-center">
+        <div className="h-full flex justify-center items-start flex-col gap-y-4 lg:gap-y-2 xl:ml-20">
+          <div className="mx-auto lg:mx-0 px-4 lg:px-0 text-6xl md:text-7xl lg:text-5xl xl:text-6xl font-bold xl:font-bold text-center lg:text-left text-neutral-600 dark:text-neutral-300 shadow-2xl w-full mb-6 xl:mb-4   ">
+            Put your systems <br /> on autopilot with AI
+          </div>
+          <div className="flex w-full text-center mb-10 text-xl xl:text-2xl lg:text-left md:justify-center lg:justify-start">
+            <p className="md:w-4/6 lg:w-5/6 px-16 lg:px-0">
+              {/* <p className="flex w-full md:w-1/2 lg:w-2/3 text-sm md:text-xl mb-10 text-center xl:text-left mx-auto md:mx-0"> */}
+              Automate your workflows, time-consuming tasks, and elevate your
+              business.
+            </p>
+          </div>
+          <div className="flex w-full justify-center lg:justify-start">
+            <LitButton buttonText="Find out how" />
+          </div>
         </div>
-        <div className="flex w-full text-center mb-10 md:text-xl xl:text-2xl xl:text-left md:justify-center lg:justify-start">
-          <p className="md:w-5/6 px-6 lg:px-0">
-            {/* <p className="flex w-full md:w-1/2 lg:w-2/3 text-sm md:text-xl mb-10 text-center xl:text-left mx-auto md:mx-0"> */}
-            Automate your workflows, time-consuming tasks, and
-            elevate your business.
-          </p>
-        </div>
-        <div className="flex w-full justify-center lg:justify-start">
-          <LitButton buttonText="Find out how" />
-        </div>
+        
       </div>
-      <div className="flex items-center">
         {!isMobile && widthChecked && <GlobeDemo />}
-      </div>
     </div>
   );
 }
